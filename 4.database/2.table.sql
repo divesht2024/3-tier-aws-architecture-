00ed8449dc7bacc1270be247e4a16fb359df11bd
Divesh Tayade <162043849+divesht2024@users.noreply.github.com>
2026-05-03 19:30:31 +0530

    Add files via upload

---
+++ 4.database/2.table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    department VARCHAR(50),
+    salary INT,
+    city VARCHAR(50)
+);
---
+++ 4.database/3.data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees (name, department, salary, city) VALUES
+('Rahul', 'IT', 45000, 'Nagpur'),
+('Priya', 'HR', 40000, 'Pune'),
+('Amit', 'IT', 55000, 'Mumbai'),
+('Sneha', 'Finance', 50000, 'Wardha'),
+('Rohit', 'IT', 60000, 'Pune');
